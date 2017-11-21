53b7067af9a0300e978cf49395a1e80e488ea4e8
mike connor <mike@rocketnumber9.org>
2017-11-21 13:31:41 -0600

    pull xml from mike’s server

--- mobile_auto_skippable/ads.js
+++ mobile_auto_skippable/ads.js
@@ -48,9 +48,9 @@
     // Request video ads.
     var adsRequest = new google.ima.AdsRequest();
     adsRequest.adTagUrl =
-     "https://mconnor.github.io/testVast/vast-icon-iframe-resource-" + postion + ".xml";
+    // "https://mconnor.github.io/testVast/vast-icon-iframe-resource-" + postion + ".xml";
       //  adsRequest.adTagUrl =
-      //   "//rocketnumber9.org/vast-test/vast-icon-iframe-resource-" + postion + ".xml";
+      "//rocketnumber9.org/vast-test/vast-icon-iframe-resource-" + postion + ".xml";
                        
     // Specify the linear and nonlinear slot sizes. This helps the SDK to
     // select the correct creative if multiple are returned.
